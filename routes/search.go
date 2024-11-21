89574fbfbcf3b196c59f70efcc1c1731708a5764
Adrian Brady <ajbrady@wisc.edu>
2024-11-21 17:48:32 -0600

    fix: remove unneeded printf debugging

--- routes/search.go
+++ routes/search.go
@@ -1,7 +1,6 @@
 package routes
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -11,9 +10,6 @@ import (
 
 func Search(c echo.Context) error {
 	query := c.FormValue("query")
-	for _, v := range c.QueryParams() {
-		log.Printf("Query: '%+v'", v)
-	}
 	if len(query) < 3 {
 		return c.String(http.StatusBadRequest, "Error 400, could not get query from request.")
 	}
