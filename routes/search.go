e30e7e5c2ff72a4c4bd4e333cadd8dc4d6d35c96
Adrian Brady <ajbrady@wisc.edu>
2024-11-25 00:46:44 -0600

    add MinQueryLength global variable

--- routes/search.go
+++ routes/search.go
@@ -8,9 +8,11 @@ import (
 	"github.com/DSSD-Madison/gmu/models"
 )
 
+const MinQueryLengh = 3
+
 func fetchSearchPage(c echo.Context) error {
 	query := c.FormValue("query")
-	if len(query) < 3 {
+	if len(query) < MinQueryLengh {
 		return c.String(http.StatusBadRequest, "Error 400, could not get query from request.")
 	}
 	return c.Render(http.StatusOK, "results-init", query)
@@ -18,7 +20,7 @@ func fetchSearchPage(c echo.Context) error {
 
 func Search(c echo.Context) error {
 	query := c.FormValue("query")
-	if len(query) < 3 {
+	if len(query) < MinQueryLengh {
 		return c.String(http.StatusBadRequest, "Error 400, could not get query from request.")
 	}
 	results := models.MakeQuery(query)
