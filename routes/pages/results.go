05a2db52c2d221985b065f8ca031d12f61aae304
bengoh815 <64095338+bengoh815@users.noreply.github.com>
2024-11-26 17:53:45 -0600

    Implemented dynamic filter render
    ARIA label might be terrible.

--- routes/pages/results.go
+++ routes/pages/results.go
@@ -6,6 +6,52 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type FilterOption struct {
+	Label	string
+	Count	int
+}
+
+type FilterCategory struct {
+	Category	string
+	Options	[]FilterOption
+}
+
+type PageData struct {
+	Where	string
+	Filters	[]FilterCategory
+}
+
 func Results(c echo.Context) error {
-	return c.Render(http.StatusOK, "results", nil)
+	filters := []FilterCategory{
+		{
+			Category: "Authors",
+			Options: []FilterOption{
+				{Label: "Search for Common Ground (SFCG)", Count: 35},
+				{Label: "The United States Agency for International Development (USAID)", Count: 32},
+				{Label: "Mercy Corps", Count: 8},
+			},
+		},
+		{
+			Category: "File Type",
+			Options: []FilterOption{
+				{Label: "PDF", Count: 391},
+				{Label: "MS_WORD", Count: 71},
+			},
+		},
+		{
+			Category: "Region",
+			Options: []FilterOption{
+				{Label: "Global", Count: 391},
+				{Label: "Nepal", Count: 71},
+			},
+		},
+	}
+
+
+	data := PageData{
+		Where: "Over here",
+		Filters: filters,
+	}
+
+	return c.Render(http.StatusOK, "results", data)
 }
