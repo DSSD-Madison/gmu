9b4a330728fa3efe6acd63adbb1b2f9fe7d4b634
Adrian Brady <ajbrady@wisc.edu>
2024-11-29 12:34:30 -0600

    Use new templating structure (#20)
    * Use new templating structure
    
    * add error handling and remove unused file
    
    ---------
    
    Co-authored-by: Sarthak Agrawal <sarthavader@gmail.com>

--- models/templates.go
+++ models/templates.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"fmt"
 	"html/template"
 	"io"
 
@@ -8,15 +9,38 @@ import (
 )
 
 type Templates struct {
-	templates *template.Template
+	templates map[string]*template.Template
 }
 
 func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
+	tmpl, ok := t.templates[name]
+	if !ok {
+		return fmt.Errorf("template %s not found", name)
+	}
+
+	err := tmpl.Execute(w, data)
+	if err != nil {
+		return fmt.Errorf("failed to render template %s: %w", name, err)
+	}
+
+	return nil
 }
 
 func NewTemplate() *Templates {
+	tmpl := make(map[string]*template.Template)
+	tmpl["index"] = template.Must(template.ParseFiles(
+		"views/index.html",
+		"views/search-home.html",
+	))
+	tmpl["search"] = template.Must(template.ParseFiles(
+		"views/search.html",
+	))
+	tmpl["results"] = template.Must(template.ParseFiles(
+		"views/results.html",
+		"views/sidecolumn.html",
+	))
+
 	return &Templates{
-		templates: template.Must(template.ParseGlob("views/*.html")),
+		templates: tmpl,
 	}
 }
