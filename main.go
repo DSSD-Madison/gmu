4037e74d1b158da2d84ba805174bc1afbc067a58
Adrian Brady <ajbrady@wisc.edu>
2025-02-12 18:53:28 -0600

    Logger (#27)
    * create new logger and implement for use
    
    * add error handle to search
    
    * add prod and dev printing modes
    
    * handle no mode env var case for logger
    
    * use new logger opts
    
    * use echo context
    
    * add logging levels from env
    
    * rename to avoid name conflicts
    
    * improve search route error message
    
    * rename Handler fields
    
    * add note to potentially replace buf with sync pool

--- main.go
+++ main.go
@@ -2,25 +2,79 @@ package main
 
 import (
 	"log"
+	"log/slog"
+	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
+	"github.com/DSSD-Madison/gmu/internal"
 	"github.com/DSSD-Madison/gmu/models"
 	"github.com/DSSD-Madison/gmu/routes"
 )
 
 func main() {
 
+	var logger *slog.Logger
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	e := echo.New()
-	e.Use(middleware.Logger())
+	mode, exist := os.LookupEnv("MODE")
+	if !exist {
+		mode = "dev"
+	}
 
+	levelStr, exist := os.LookupEnv("LOG_LEVEL")
+	var level slog.Level
+	switch levelStr {
+	case "debug":
+		level = slog.LevelDebug
+	case "info":
+		level = slog.LevelInfo
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
+
+	loggerOpts := internal.HandlerOptions{
+		Mode:  mode,
+		Level: level,
+	}
+
+	logger = slog.New(internal.NewHandler(&loggerOpts))
+
+	e := echo.New()
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus:   true,
+		LogURI:      true,
+		LogError:    true,
+		LogRemoteIP: true,
+		HandleError: true, // forwards error to the global error handler, so it can decide appropriate status code
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			if v.Error == nil {
+				logger.LogAttrs(c.Request().Context(), slog.LevelInfo, "REQUEST",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+					slog.String("ip", v.RemoteIP),
+				)
+			} else {
+				logger.LogAttrs(c.Request().Context(), slog.LevelError, "REQUEST ERROR",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+					slog.String("err", v.Error.Error()),
+					slog.String("ip", v.RemoteIP),
+				)
+			}
+			return nil
+		},
+	}))
 	// Static file handlers
 	e.Static("/images", "static/images")
 	e.Static("/css", "static/css")
