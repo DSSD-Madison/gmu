a8e8ba743a8d3067dcc6e6a20fc4e50efdf6295f
Adrian Brady <ajbrady@wisc.edu>
2025-03-12 22:32:10 -0500

    add queue

--- routes/search.go
+++ routes/search.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"net/http"
+	"sync/atomic"
+	"time"
 
 	"github.com/labstack/echo/v4"
 
@@ -24,7 +26,36 @@ func SearchSuggestions(c echo.Context) error {
 	return c.Render(http.StatusOK, "suggestions", suggestions)
 }
 
+type Job struct {
+	ID       int
+	Response chan models.KendraResults
+}
+
+const maxWorkers = 2
+
+var (
+	activeWorkers int32
+	semaphore     = make(chan struct{}, maxWorkers)
+)
+
+func worker(job Job, query string) {
+	defer func() {
+		<-semaphore
+		atomic.AddInt32(&activeWorkers, -1)
+	}()
+
+	// fmt.Printf("worker processing job: %d", job.ID)
+	// time.Sleep(2 * time.Second)
+	// result := fmt.Sprintf("Job %d completed!", job.ID)
+	// fmt.Printf("worker finished job %d\n", job.ID)
+
+	result := models.MakeQuery(query, nil)
+
+	job.Response <- result
+}
+
 func Search(c echo.Context) error {
+
 	query := c.FormValue("query")
 	context := c.FormValue("context")
 
@@ -34,7 +65,23 @@ func Search(c echo.Context) error {
 	if context == "home" {
 		return c.Render(http.StatusOK, "search", query)
 	}
-	results := models.MakeQuery(query, nil)
+
+	jobID := time.Now().UnixNano()
+	respChan := make(chan models.KendraResults, 1)
+	job := Job{
+		ID:       int(jobID),
+		Response: respChan,
+	}
+
+	semaphore <- struct{}{}
+	atomic.AddInt32(&activeWorkers, 1)
+
+	go worker(job, query)
+
+	results := <-respChan
+
+	// results := models.MakeQuery(query, nil)
 
 	return c.Render(http.StatusOK, "results", results)
+	// return c.String(http.StatusOK, result)
 }
