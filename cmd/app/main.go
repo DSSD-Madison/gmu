289a28e65a7261c978f2da8004211f9e42eb8ac1
Adrian Brady <ajbrady@wisc.edu>
2025-03-22 17:36:01 -0500

    restructure files

---
+++ cmd/app/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+
+	 _ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/DSSD-Madison/gmu/pkg/logger"
+	"github.com/DSSD-Madison/gmu/routes"
+	"github.com/DSSD-Madison/gmu/pkg/db"
+)
+
+func main() {
+	var l *slog.Logger
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	mode, exist := os.LookupEnv("MODE")
+	if !exist {
+		mode = "dev"
+	}
+
+	levelStr, exist := os.LookupEnv("LOG_LEVEL")
+	var level slog.Level
+	switch levelStr {
+	case "debug":
+		level = slog.LevelDebug
+	case "info":
+		level = slog.LevelInfo
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
+
+	loggerOpts := logger.HandlerOptions{
+		Mode:  mode,
+		Level: level,
+	}
+
+	l = slog.New(logger.NewHandler(&loggerOpts))
+
+	e := echo.New()
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus:   true,
+		LogURI:      true,
+		LogError:    true,
+		LogRemoteIP: true,
+		HandleError: true, // forwards error to the global error handler, so it can decide appropriate status code
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			if v.Error == nil {
+				l.LogAttrs(c.Request().Context(), slog.LevelInfo, "REQUEST",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+					slog.String("ip", v.RemoteIP),
+				)
+			} else {
+				l.LogAttrs(c.Request().Context(), slog.LevelError, "REQUEST ERROR",
+					slog.String("uri", v.URI),
+					slog.Int("status", v.Status),
+					slog.String("err", v.Error.Error()),
+					slog.String("ip", v.RemoteIP),
+				)
+			}
+			return nil
+		},
+	}))
+
+	host := os.Getenv("PROD_HOST")
+	user := os.Getenv("PROD_USER")
+	dbname := os.Getenv("PROD_DB")
+	password := os.Getenv("PROD_PASSWORD")
+
+	if host == "" || user == "" || dbname == "" || password == "" {
+		log.Fatal("Database environment variables are not set properly")
+	}
+
+	databaseURL := fmt.Sprintf(
+		"postgres://%s:%s@%s/%s?sslmode=disable",
+		user, password, host, dbname,
+	)
+
+	// Connect to PostgreSQL using pgxpool
+	dbpool, err := pgxpool.Connect(context.Background(), databaseURL)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+	defer dbpool.Close()
+
+	// Create a *sql.DB instance using the pgx driver
+	sqlDB, err := sql.Open("pgx", databaseURL)
+	if err != nil {
+		log.Fatalf("Unable to initialize sql.DB: %v", err)
+	}
+	defer sqlDB.Close()
+
+	db_querier := db.New(sqlDB)
+
+	// Static file handlers
+	e.Static("/images", "web/assets/images")
+	e.Static("/css", "web/assets/css")
+	e.Static("/svg", "web/assets/svg")
+
+	// Routes
+	routes.InitRoutes(e, db_querier)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
+}
---
+++ pkg/db/handlers/get.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"context"
+
+	"github.com/labstack/echo/v4"
+
+	"github.com/DSSD-Madison/gmu/pkg/db"
+)
+
+type DBDocumentQuerier interface {
+	GetDocumentsByURIs(ctx context.Context, uris []string) ([]db.Document, error)
+}
+
+func GetDocuments(c echo.Context, queries *db.Queries, uris []string) (map[string]db.Document, error) {
+    documents, err := queries.GetDocumentsByURIs(c.Request().Context(), uris)
+    if err != nil {
+        return nil, err
+    }
+
+    documentMap := make(map[string]db.Document)
+
+    for _, document := range documents {
+        documentMap[document.S3File] = document
+    }
+
+    return documentMap, nil
+}
