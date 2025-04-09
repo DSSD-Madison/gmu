e0c822541f59fbad77ecd156e724a4e06691d32f
Adrian Brady <ajbrady@wisc.edu>
2025-04-08 19:53:41 -0500

    update margin of navbar content

--- web/components/navbar_templ.go
+++ web/components/navbar_templ.go
@@ -29,7 +29,7 @@ func Navbar() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<nav class=\"dark:bg-gray-800\"><div class=\"mx-auto max-w-7xl px-2\"><div class=\"relative flex h-16 items-center justify-between\"><div class=\"flex flex-1 items-center justify-center sm:items-stretch sm:justify-start\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<nav class=\"dark:bg-gray-800\"><div class=\"mx-5 max-w-7xl px-2\"><div class=\"relative flex h-16 items-center justify-between\"><div class=\"flex flex-1 items-center justify-center sm:items-stretch sm:justify-start\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
