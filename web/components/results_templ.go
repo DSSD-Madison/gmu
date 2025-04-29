beff2a43de3a9b6db8d23f4ca67a7032f79afa8f
Adrian Brady <mail@adrianbrady.com>
2025-04-28 21:35:35 -0500

    fix extra searchbar (#89)

--- web/components/results_templ.go
+++ web/components/results_templ.go
@@ -56,7 +56,7 @@ func ResultsPage(results awskendra.KendraResults, isAuthorized bool) templ.Compo
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = Searchbar(results.UrlData, isAuthorized).Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = Searchbar(results.UrlData, true).Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
